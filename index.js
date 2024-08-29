0c01d525d204cbf15b245922279a88154dd40a91
claudiomss <claudiocld95@gmail.com>
2024-08-29 12:46:36 -0300

    add pixel oficial

--- index.js
+++ index.js
@@ -76,8 +76,8 @@ app.post("/cok", cors(), async (req, res) => {
 async function sendPurchaseEvent(data) {
   const axios = require("axios")
   const accessToken =
-    "EAAGmZBgCmBa0BOZCpy3qZAeJEbMPspePaZA04Qfr4fHQ9srxnSFZCOjPDAZAyHIwAlzmhdLcKzfmAj8bjM50pTEWvJLZBB4CwE0oWZCji3mVCsTtob0Gy2pub4azWwyWKzEy1ZCf8v2HzX9n0Cl66yN1wPk5bIUZBB4b51qvSJpVMg6siwdyBymMqgoZAXZC6LotctdCMgZDZD"
-  const pixelId = "1011636240246252"
+    "EAAHqLIBp79EBO7M9JQZCVvi0U6H56pSEY6R3eba6CckuB4Rpl7COpvVUQjk2n4SM2rBkn700upiWMQjzhEnUPfjLbWfqzZBT6gGBlTlRgiNynZAIvBYCGNLtOqzFwkiZACZADs9XZCw2EDOtDxUFbZAIJwiLtuTbnKCq98lm5iNZCKB7m0QFq49p0Czu0z52UZBXq9QZDZD"
+  const pixelId = "4636511609906695"
   const url = `https://graph.facebook.com/v13.0/${pixelId}/events`
 
   const eventData = {
